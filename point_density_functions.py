cf7ae3d754b5bfa927fd109236d549150dfee292
Michael <michael.hamer.stanley@gmail.com>
2020-04-07 09:50:12 -0400

    vertical density, statistics, replacing 5 commits

--- point_density_functions.py
+++ point_density_functions.py
@@ -67,7 +67,8 @@ def grab_points(pt_files,file_dir,pt_x,pt_y,feet_from_point):
     square_points = pd.DataFrame()
     for pick in pt_files:
         las_points = pd.read_hdf(file_dir+pick)
-        las_points['flight_id'] = pick[11:-3]
+        if 'flight_id' not in las_points.columns:
+            las_points['flight_id'] = pick[11:-3]
         new_square_points = las_points[ (las_points['x_scaled'] < pt_x + feet_from_point)
                 &(las_points['x_scaled'] > pt_x - feet_from_point) 
                 &(las_points['y_scaled'] < pt_y + feet_from_point)
@@ -85,11 +86,12 @@ def grab_points(pt_files,file_dir,pt_x,pt_y,feet_from_point):
 def grab_points_big_rect(pt_files,file_dir,uv_inv,w):
     '''
     Function extracts all points in all pt_files within feet_from_point of (pt_x,pt_y)
+    Note: This function currently only works in 2-D (horizontal plane)
     Inputs:
         pt_files - List of strings, filenames of .lz files (created by create_df_hd5 function)
         file_dir - String, directory name containing pt_files
-        pt_x,pt_y - Float, X and Y coordinate of the center point of the desired output
-        feet_from_point - Float, how many feet in each coordinate direction to allow
+        uv_inv - 
+        w - 
         
     Output:
         square_points - DataFrame, contains only points within the bounds described, full LAS fields
@@ -97,7 +99,8 @@ def grab_points_big_rect(pt_files,file_dir,uv_inv,w):
     rectangle_points = pd.DataFrame()
     for pick in pt_files:
         las_points = pd.read_hdf(file_dir+pick)
-        las_points['flight_id'] = pick[11:-3]
+        if 'flight_id' not in las_points.columns:
+            las_points['flight_id'] = pick[11:-3]
         unit_square = (las_points[['x_scaled','y_scaled']]-w)@(uv_inv.T)
         new_rectangle_points = las_points[(unit_square[0]<=1) & (unit_square[0]>=0) & (unit_square[1]<=1) & (unit_square[1]>=0)]
         print("Point count in new square from {:s}: {:d}".format(pick,new_rectangle_points.shape[0]))
@@ -131,8 +134,7 @@ def plane_fit(square_points):
     # Project each point onto the plane
     proj_on_norm = dist_from_plane*np.array([norm_vector]).T
     pts_on_plane = points - proj_on_norm.T
-    
-    square_points.loc['dist_from_plane'] = dist_from_plane
+    square_points.loc[:,'dist_from_plane'] = dist_from_plane
     
     return norm_vector,points,square_points,pts_on_plane
 
@@ -146,7 +148,7 @@ def prep_square_for_plotting(square_points,min_list=None):
         square_points - Same DataFrame with x_plot,y_plot,z_plot fields added (_scaled field less the min value)
         min_list - [x_min,y_min,z_min] values used to create _plot fields
     '''
-    square_points['size_num'] = 1 # added for plotting
+    square_points.loc[:,'size_num'] = 1 # added for plotting
     if min_list:
         x_min = min_list[0]
         y_min = min_list[1] 
@@ -162,49 +164,57 @@ def prep_square_for_plotting(square_points,min_list=None):
     return square_points, min_list
 
 
-def grab_wall_face(square_points,pt_1,pt_2,z_low,z_high,epsilon=1e-2):
+def grab_wall_face(square_points,norm_vector,pt_1,z_low,z_high,epsilon=1e-2):
     '''
     Function extracts points from square_points dataframe that are in the x-y line defined by 2 points (+/-epsilon)
     and between [z_low,z_high] in the vertical.
     Inputs:
         square_points - dataframe with x_plot,y_plot,z_plot
-        pt_1,pt_2 - 2-tuple of xy coordinates of 2 points in the wall
+        norm_vector - 3x1 numpy array of the normal vector to the plane
+        pt_1 - 3-tuple of xyz coordinate of a point in the wall
         z_low,z_high - scalars indicating the range of vertical
-        epsilon - scalar, indicating the allowable deviation from the xy-slope (note: this is a sloppy way to handle range)
+        epsilon - scalar, indicating the allowable point distance from the plane defined by norm_vector and pt_1
     Output:
         wall_face - dataframe subset of square_points satisfying the above criteria
     '''
-    m = (pt_2[1]-pt_1[1])/(pt_2[0]-pt_1[0])
-    off_line = abs((square_points['y_plot'] - pt_1[1]) / (square_points['x_plot']-pt_1[0]) - m)
-    wall_face = square_points[(off_line<epsilon)&(square_points['z_plot']>z_low) & (square_points['z_plot']<z_high)]
+    xyz_array = square_points[['x_scaled','y_scaled','z_scaled']]
+    dist_from_plane = norm_vector@(xyz_array - pt_1).T
+    wall_face = square_points[(abs(dist_from_plane)<epsilon)&(square_points['z_scaled']>z_low) & (square_points['z_scaled']<z_high)]
     return wall_face
 
-def vertical_point_density(square_points,norm_vector,wall_pt,feet_from_pt):
+def in_vertical_square(square_points,norm_vector,center_pt,horizontal_feet_from_pt, vertical_feet_from_pt):
     '''
-    Function counts the number of points in the (2*feet_from_pt)**2 sqft vertical space.
-    Allowed distance in the xy plane is determined by the normal vector to the planar normal vector.
+    Function counts the number of points in the (2*horizontal_feet_from_pt)*(2*vertical_feet_from_pt) sqft vertical space.
+    Allowed distance in the xy plane is determined by the normal vector to the planar normal vector.  Points are projected onto
+    plane and then the xy dist is calculated.
     Inputs:
         square_points - DataFrame containing x_plot,y_plot,z_plot fields
         norm_vector - 3D numpy array of xyz directions, norm = 1
-        wall_pt - 3-tuple of xyz points
+        center_pt - 3-tuple of xyz point, center of rectangle
         feet_from_pt - float, how many feet in each coordinate direction to allow
     Output:
         Prints vertical density
     '''
     xy_vector = np.array([-norm_vector[1],norm_vector[0]])
     xy_vector = xy_vector / np.linalg.norm(xy_vector) # Norm to 1
-    wall_pt_x = wall_pt[0]
-    wall_pt_y = wall_pt[1]
-    wall_pt_z = wall_pt[2]
-    vertical_square = square_points[(square_points['x_plot']<wall_pt_x+feet_from_pt*abs(xy_vector[0]))
-                                        &(square_points['x_plot']>wall_pt_x-feet_from_pt*abs(xy_vector[0])) &
-                                         (square_points['y_plot']<wall_pt_y+feet_from_pt*abs(xy_vector[1])) & 
-                                         (square_points['y_plot']>wall_pt_y-feet_from_pt*abs(xy_vector[1])) & 
-                                         (square_points['z_plot']<wall_pt_z+feet_from_pt) &
-                                         (square_points['z_plot']>wall_pt_z-feet_from_pt)]
-    density = vertical_square.shape[0]/((feet_from_pt*2)**2)
-    print("Vertical density: {:2.3f} pts/sqft".format(density))
 
+
+    # Project each point onto the plane
+    orth_component = [dist*norm_vector for dist in square_points['dist_from_plane']]
+    proj_on_plane = square_points[['x_scaled','y_scaled','z_scaled']] - np.array(orth_component)
+
+    xy_dist_from_center_pt = np.linalg.norm(proj_on_plane[['x_scaled','y_scaled']] - center_pt[:2],axis=1)
+    
+    vertical_square = square_points[(xy_dist_from_center_pt<horizontal_feet_from_pt)
+                                        &(square_points['z_scaled']<center_pt[2]+vertical_feet_from_pt) &
+                                        (square_points['z_scaled']>center_pt[2]-vertical_feet_from_pt)]
+
+
+    rect_area = ((vertical_feet_from_pt*2)*(horizontal_feet_from_pt*2))
+    density = vertical_square.shape[0]/rect_area
+    print("Vertical density: {:2.3f} pts/sqft".format(density))
+    print("Rectangle area: {} sqft".format(rect_area))
+    return vertical_square
 
 def label_returns(las_df):
     '''
